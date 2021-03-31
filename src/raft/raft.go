19d6a0d79ac5031a8db93d81527988a4b8f66d6e
Hsin-Hung Wu <henrywu19970511@gmail.com>
2021-04-01 04:29:17 +0800

    fix conflict indexing

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -1167,11 +1167,11 @@ func (rf *Raft) sendAppendEntries(server int){
 			found := false
 
 			// if an entry is found with this conflict term
-			for i := len(rf.log)-1 ; i>0 ; i--{
+			for i := len(rf.log)-1 ; i>=0 ; i--{
 
 				if rf.log[i].Term == reply.ConflictTerm{
 
-					rf.nextIndex[server] = min(rf.convert2ActualIndex(i), reply.ConflictIndex) 
+					rf.nextIndex[server] = rf.convert2ActualIndex(i) //min(rf.convert2ActualIndex(i), reply.ConflictIndex) 
 					found = true
 					break
 				}
@@ -1189,7 +1189,6 @@ func (rf *Raft) sendAppendEntries(server int){
 
 		}	
 
-		rf.matchIndex[server] = rf.nextIndex[server] - 1
 
 	}
 }
