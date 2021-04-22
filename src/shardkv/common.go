05509da4769f3e22ff776174f8658ccb2b580022
Hsin-Hung Wu <henrywu19970511@gmail.com>
2021-04-23 04:11:31 +0800

    copy kvraft

--- src/shardkv/common.go
+++ src/shardkv/common.go
@@ -14,6 +14,7 @@ const (
 	ErrNoKey       = "ErrNoKey"
 	ErrWrongGroup  = "ErrWrongGroup"
 	ErrWrongLeader = "ErrWrongLeader"
+	ErrTimeOut     = "ErrTimeOut"
 )
 
 type Err string
@@ -27,6 +28,8 @@ type PutAppendArgs struct {
 	// You'll have to add definitions here.
 	// Field names must start with capital letters,
 	// otherwise RPC will break.
+	ClientID int64
+	SerialID int64
 }
 
 type PutAppendReply struct {
@@ -36,6 +39,8 @@ type PutAppendReply struct {
 type GetArgs struct {
 	Key string
 	// You'll have to add definitions here.
+	ClientID int64
+	SerialID int64
 }
 
 type GetReply struct {
